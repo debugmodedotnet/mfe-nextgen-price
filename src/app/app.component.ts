f93b54ec2f7e22c6b7da0ff3be55478e23e37a6a
Ariba <ariba.madakia@gmail.com>
2024-08-09 12:26:36 +0530

    Added Kendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GridModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'price';
+
+  public gridData: any[] = [
+    { id: 1, name: 'John Doe', age: 30, city: 'New York' },
+    { id: 2, name: 'Jane Smith', age: 25, city: 'San Francisco' },
+    { id: 3, name: 'Michael Johnson', age: 35, city: 'Los Angeles' }
+  ];
 }
